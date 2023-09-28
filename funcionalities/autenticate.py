8a05157997e25e269b43caeaaba04ae13128e52e
Fercarllorens <79652574+Fercarllorens@users.noreply.github.com>
2023-09-28 16:42:15 +0200

    Update autenticate.py

--- funcionalities/autenticate.py
+++ funcionalities/autenticate.py
@@ -5,10 +5,10 @@ import tweepy
 Función utilizada para utilizar la Api de Twitter
 '''
 def get_auth():
-    consumer_key = 'qDqiLQoTgQusOGa9x0YJJtXr8'
-    consumer_secret = 'HBb2eOxW1XbO1ERIul0UR0zN3MDNz4P1IF6UKqA1qjdXcSBVL4'
-    access_token = '2792071239-gwhCrTsicTXttl5Z9gk1YMYilZ7kybn1fJDaTiS'
-    access_token_secret = 'hlOYQZ47LU6eeyx9T3nAaTrixz4hJTFtEMOkwFF0yxfAG'
+    consumer_key = 'X'
+    consumer_secret = 'X'
+    access_token = 'X'
+    access_token_secret = 'X'
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     return auth
